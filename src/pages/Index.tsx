89e5ec3d1fe464d279faed3cf35b0839a505910d
koushikikundu9 <kundukoushiki73@gmail.com>
2025-04-19 20:18:05 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -142,7 +142,7 @@ const Index = () => {
           
           <Link to="/calm">
             <Button 
-              className="w-full bg-care-DEFAULT hover:bg-care-dark text-white"
+              className="w-full bg-care-dark hover:bg-care-light text-white hover:text-care-dark transition-all duration-300"
             >
               <HeartPulse className="mr-2 h-5 w-5" />
               Need Calm Now
